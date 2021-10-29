860335ec316420a3a4b95fb0d09d363aad5ccc5e
AntikBLR <86202029+AntikBLR@users.noreply.github.com>
2021-10-29 22:39:51 +0300

    git add test untils

--- components/Untils/utils.test.jsx
+++ components/Untils/utils.test.jsx
@@ -4,4 +4,8 @@ describe('utils', () => {
 	test('formatUserName adds @ at the beginning of the username', () => {
 		expect(formatUserName('jc')).toBe('@jc')
 	})
+
+	test('formatUserName does not add @ when it is already provided', () => {
+		expect(formatUserName('@jc')).toBe('@jc')
+	})
 })
