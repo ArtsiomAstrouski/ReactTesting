b600718d857b2e673abf4ed60d7aff2a1fbaaaa3
AntikBLR <86202029+AntikBLR@users.noreply.github.com>
2021-10-31 21:06:42 +0300

    components moved to folder scr

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import { formatUserName } from './utils'
+import { formatUserName } from './components/Untils/untils'
 import './App.css'
 
 function App() {
