e9e763423ad5ad14192becb72943cf225adcc9a5
AntikBLR <86202029+AntikBLR@users.noreply.github.com>
2021-10-31 21:22:22 +0300

    added home page

--- src/App.js
+++ src/App.js
@@ -1,47 +1,22 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import { formatUserName } from './components/Untils/untils'
 import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './components/Home/home'
+import HiddenMessage from './components/HiddenMessage/hiddenMessage'
 
 function App() {
-	const [users, setUsers] = useState([])
-
-	// Load the data from the server
-	useEffect(() => {
-		let mounted = true
-
-		const getUsers = async () => {
-			const response = await axios.get(
-				'https://jsonplaceholder.typicode.com/users'
-			)
-			if (mounted) {
-				setUsers(response.data)
-			}
-		}
-
-		getUsers()
-
-		return () => {
-			mounted = false
-		}
-	}, [])
-
 	return (
-		<div className="App">
-			<div>Users:</div>
-			{users.length ? (
-				<ul data-testid="user-list">
-					{users.map(user => (
-						<li key={user.id} className="user" data-testid="user-item">
-							<span>{user.name}</span> (
-							<span>{formatUserName(user.username)}</span>)
-						</li>
-					))}
-				</ul>
-			) : (
-				<div>Loading users...</div>
-			)}
-		</div>
+		<Router>
+			<Switch>
+				<Route exact path="/" render={() => <Home nav={Home} />} />
+				<Route
+					exact
+					path="/hidden"
+					render={() => <HiddenMessage nav={HiddenMessage} />}
+				/>
+				<Route exact path="/" render={() => <Home nav={Home} />} />
+				{/* <Route component={Error} /> */}
+			</Switch>
+		</Router>
 	)
 }
 
