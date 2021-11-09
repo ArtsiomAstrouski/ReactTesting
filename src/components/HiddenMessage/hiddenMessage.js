414c3d4d61078c6b7253de28b2b344607762a3ec
AntikBLR <86202029+AntikBLR@users.noreply.github.com>
2021-11-09 19:20:50 +0300

    code refactoring HiddenMessage page

--- src/components/HiddenMessage/hiddenMessage.js
+++ src/components/HiddenMessage/hiddenMessage.js
@@ -1,21 +1,74 @@
 import * as React from 'react'
 import Header from '../Header/header'
+import { ReactComponent as Danger } from 'public/danger.svg'
+import { makeStyles } from '@material-ui/core/styles'
+import Modal from '@material-ui/core/Modal'
 
-function HiddenMessage({ children }) {
+function rand() {
+	return Math.round(Math.random() * 20) - 10
+}
+
+function getModalStyle() {
+	const top = 50 + rand()
+	const left = 50 + rand()
+
+	return {
+		top: `${top}%`,
+		left: `${left}%`,
+		transform: `translate(-${top}%, -${left}%)`,
+	}
+}
+
+const useStyles = makeStyles(theme => ({
+	paper: {
+		position: 'absolute',
+		width: 400,
+		backgroundColor: theme.palette.background.paper,
+		border: '2px solid #000',
+		boxShadow: theme.shadows[5],
+		padding: theme.spacing(2, 4, 3),
+	},
+}))
+
+export default function HiddenMessage({ children }) {
 	const [showMessage, setShowMessage] = React.useState(false)
+	const classes = useStyles()
+	// getModalStyle is not a pure function, we roll the style only on the first render
+	const [modalStyle] = React.useState(getModalStyle)
+	const [open, setOpen] = React.useState(false)
+
+	const handleOpen = () => {
+		setOpen(true)
+	}
+
+	const handleClose = () => {
+		setOpen(false)
+	}
+
 	return (
 		<div>
-			<Header />
-			<label htmlFor="toggle">Show Message</label>
-			<input
-				id="toggle"
-				type="checkbox"
-				onChange={e => setShowMessage(e.target.checked)}
-				checked={showMessage}
-			/>
-			{showMessage ? children : null}
+			<button type="button" onClick={handleOpen}>
+				Open Modal
+			</button>
+			<Modal
+				open={open}
+				onClose={handleClose}
+				aria-labelledby="simple-modal-title"
+				aria-describedby="simple-modal-description"
+			>
+				<div>
+					<Header />
+					<label htmlFor="toggle">Show Message</label>
+					<input
+						id="toggle"
+						type="checkbox"
+						onChange={e => setShowMessage(e.target.checked)}
+						checked={showMessage}
+					/>
+					{showMessage ? children : null}
+				</div>
+				<div>xx</div>
+			</Modal>
 		</div>
 	)
 }
-
-export default HiddenMessage
