ad521485c67b8e9043a3f07c0e73312e21a9afb7
AntikBLR <86202029+AntikBLR@users.noreply.github.com>
2021-11-24 21:32:33 +0300

    code refactor

--- src/test.js
+++ src/test.js
@@ -1,38 +1,32 @@
-// import React from 'react'
-// import userEvent from '@testing-library/user-event'
-// import {
-//   render,
-//   screen,
-//   fireEvent,
-// } from '@testing-library/react'
-// import '@testing-library/jest-dom'
+import React from 'react'
+import userEvent from '@testing-library/user-event'
+import { render, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
 
-// import { MuiThemeProvider } from '@material-ui/core/styles'
+import { MuiThemeProvider } from '@material-ui/core/styles'
 
-// describe('Test component', () => {
-//   it('should render', () => {
-//     const mockCallback = jest.fn();
+describe('Test component', () => {
+	it('should render', () => {
+		const mockCallback = jest.fn()
 
-//     const test = render(
-//       <MuiThemeProvider>
-//         <Test handleRemove={mockCallback}>
-//           {'testLabel'}
-//         </Test>
-//       </MuiThemeProvider>
-//     )
+		const test = render(
+			<MuiThemeProvider>
+				<Test handleRemove={mockCallback}>{'testLabel'}</Test>
+			</MuiThemeProvider>
+		)
 
-//     expect(test).toMatchSnapshot()
-// 		const button = test.container.querySelector('button')
-//   })
+		expect(test).toMatchSnapshot()
+		const button = test.container.querySelector('button')
+	})
 
-// 	it('render with children', () => {
-// 		const handler = jest.fn();
+	it('render with children', () => {
+		const handler = jest.fn()
 
-// 		render(<ButtonTest onClick={handler}>Some text</ButtonTest>)
-// 		expect(screen.getByText('Some text')).toBeInTheDocument()
-// 	})
-// })
-// 	expect(screen.queryByRole('button')).toBeNull()
-// 	userEvent.click(button)
-//  expect(mockCallback).toBeCalled()
-// screen.debug()
+		render(<ButtonTest onClick={handler}>Some text</ButtonTest>)
+		expect(screen.getByText('Some text')).toBeInTheDocument()
+	})
+})
+expect(screen.queryByRole('button')).toBeNull()
+userEvent.click(button)
+expect(mockCallback).toBeCalled()
+screen.debug()
